48588b00299c1c85893e74dfde8fe5cb4737fadd
IsabelCA01 <castrillon.isabel@uces.edu.co>
2023-02-01 10:49:49 -0500

    Cambios primera y segunda clase

---
+++ Transformaciones.py
@@ -0,0 +1,9 @@
+#Ejercicio 1
+import cv2 as cv
+import numpy as np
+
+S = 100
+
+def im_1():
+    im=np.zeros([100,100])
+    
---
+++ clase1.py
@@ -0,0 +1,20 @@
+#Ejercicios de clase 1
+
+import cv2 as cv
+import numpy as np
+
+S = 100
+
+def im_1():
+    im=np.zeros([100,100])
+    for i in range(S):
+        im[i,i] = 255
+    
+    cv.imwrite("imagen1.jpg",im)
+
+
+im_1()
+
+
+
+
